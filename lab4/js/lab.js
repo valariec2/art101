c12caf9e53954a77c0cd4ab4a7c53398c2e620ff
valvalsitu <vsitu@ucsc.edu>
2025-04-19 18:37:34 -0700

    Final Code 4/19

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,8 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Rock Paper Scissors
+// An implementation of the game
+// Author: Valarie Situ
+//Contact: vsitu@ucsc.edu
+// Date: April 21, 2024
 
 // Constants
 
@@ -19,3 +21,13 @@ function main() {
 
 // let's get this party started
 main();
+
+//1. Find one opponent
+//2. Print icons (rock, paper, and scissor buttons)
+//3. Prompt user selection with 3,2,1 countdown
+//4. Accept user input
+  //Rock wins Scissors
+  //Scissors wins Paper
+  //Paper wins Rock
+  //If player choose same icon, do a replay of the round.
+//5. Evaluate win or lose
